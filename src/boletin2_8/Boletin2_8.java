a142f937caca4ae46b05e039404d4f896ea8c1db
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2017-10-10 08:44:43 +0200

    Inicio proyecto

---
+++ src/boletin2_8/Boletin2_8.java
@@ -0,0 +1,35 @@
+
+package boletin2_8;
+
+import java.util.Scanner;
+
+
+public class Boletin2_8 {
+
+   
+ public static void main(String[] args) {
+    
+        Scanner scan=new Scanner(System.in);
+        int total;
+        
+        
+        System.out.println("Introduce billetes de 100");
+        total=scan.nextInt()*100;
+        
+        System.out.println("Introduce billetes de 20");
+        total=total+scan.nextInt()*20;
+        
+        System.out.println("Introduce billetes de 5");
+        total=total+scan.nextInt()*5;
+        
+        System.out.println("Introduce moedas");
+        total=total+scan.nextInt();
+        
+        
+System.out.println("Total= "+total+"€");  
+          
+          
+          
+    }
+    
+}
